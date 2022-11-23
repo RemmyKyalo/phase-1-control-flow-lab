f378815852ba6ad2cab86839363d19d9f09c893a
remmy <chaloremmygmail.comgit config --global user.name remmygit config --global user.email chaloremmygmail.com>
2022-11-23 12:46:04 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  if(feet<=400){
+    return 'This one is on me!';
+  }
+  else if(feet>=2000 && feet<=2500){
+    return 'I will gladly take your thirty bucks.';
+
+  }
+  else if (feet>=2500){
+    return 'No can do.';
+  }
+}
+scuberGreetingForFeet(199);
+scuberGreetingForFeet(2001);
+scuberGreetingForFeet(2501);
+
+
+function ternaryCheckCity(city){
+  // Write your code here!
+  const result = (city== 'NYC') ?  "Ok, sounds good." : "No go." ;
+  return result ;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+ternaryCheckCity('Pittsburgh');
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch(tip){
+    case "generous":
+      return "Thank you so much.";
+    break;
+    case "not as generous":
+      return "Thank you.";
+    break;
+    default :
+    return "Bye.";
+    
+  }
+
 }
+switchOnCharmFromTip('generous');
+switchOnCharmFromTip('not as generous');
+switchOnCharmFromTip('thanks for everything');
